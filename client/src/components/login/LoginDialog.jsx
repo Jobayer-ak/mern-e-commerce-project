6d29bf2d005f26c6f2ccba50c0c7a4101a903043
Jobayer_ak <jobayer.ak@gmail.com>
2022-07-24 00:20:09 +0600

    working on login

---
+++ client/src/components/login/LoginDialog.jsx
@@ -0,0 +1,12 @@
+
+import { Dialog } from '@mui/material';
+import React from 'react';
+
+const LoginDialog = () => {
+    return (
+        <Dialog open={true}>
+            Hello
+        </Dialog>
+};
+
+export default LoginDialog;
