b0b2f1ecf3230701db2a960a40a14966f94ed01e
Jobayer_ak <jobayer.ak@gmail.com>
2022-07-24 12:47:38 +0600

    created user-controller file for backend api

---
+++ server/controller/user-controller.js
@@ -0,0 +1,9 @@
+
+
+export const userSignup = (req, res) =>{
+    try{
+        console.log(req.body);
+    }catch(err){
+        console.log(err.message)
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import Connection from "./database/db.js"; // we must write filename with extension for backend
-import defaultData from "./default.js"
+import defaultData from "./default.js";
+import Router from "./routes/route.js";
 
 dotenv.config();
 const app = express();
+app.use(cors());
+
+app.use("/", Router)
 
 const PORT = 5000;
 
