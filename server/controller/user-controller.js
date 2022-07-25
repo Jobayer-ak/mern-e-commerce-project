84f42e7d338675b74ea1f064d354af58ab490931
Jobayer_ak <jobayer.ak@gmail.com>
2022-07-25 15:00:27 +0600

    login api created and working well

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,18 +1,15 @@
 import { response } from "express";
 import User from "../model/user-schema.js";
 
-
-
-
+// signup api
 export const userSignup = async (req, res) => {
   try {
+    const exist = await User.findOne({ username: req.body.username });
 
-    const exist = await User.findOne({username: req.body.username});
-
-    if(exist){
-        return res.status(401).json({
-            message: "Username already exists"
-        })
+    if (exist) {
+      return res.status(401).json({
+        message: "Username already exists",
+      });
     }
 
     const user = req.body;
@@ -20,11 +17,28 @@ export const userSignup = async (req, res) => {
     await newUser.save();
 
     res.status(200).json({
-        message: user
-    })
+      message: user,
+    });
   } catch (err) {
     res.status(500).json({
-        message: err.message
+      message: err.message,
     });
+  }
+};
+
+// login api
+export const userLogin = async (req, res) => {
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let user = await User.findOne({ username: username, password: password });
+    if (user) {
+      return res.status(200).json({ data: user });
+    } else {
+      return res.status(401).json("Invalid login"); // 401 means invalid request
+    }
+  } catch (err) {
+    res.status(500).json("Error ", err.message);
   }
 };
