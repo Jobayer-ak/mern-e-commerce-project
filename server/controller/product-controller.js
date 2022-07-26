7fcb161b5c4198fd27620f924da96b078945f3d7
Jobayer_ak <jobayer.ak@gmail.com>
2022-07-27 00:44:56 +0600

    used redux

--- server/controller/product-controller.js
+++ server/controller/product-controller.js
@@ -1,12 +1,14 @@
 
 import Product from "../model/product-schema.js";
 
-export const getProducts = (req, res) =>{
+export const getProducts = async (req, res) =>{
     try{
+        const products = await Product.find({});
 
+        res.status(200).json(products)
     }
     catch(err){
-
+        res.status(500).json({message: err.message});
     }
 }
 
