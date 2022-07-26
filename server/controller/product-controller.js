4788e96ac063ea47182fe7e4fffdae0ba2261338
Jobayer_ak <jobayer.ak@gmail.com>
2022-07-26 19:27:21 +0600

    array.find() method details are written inside product-controller.js file

---
+++ server/controller/product-controller.js
@@ -0,0 +1,18 @@
+
+import Product from "../model/product-schema.js";
+
+export const getProducts = (req, res) =>{
+    try{
+
+    }
+    catch(err){
+
+    }
+}
+
+// array.find() method
+// 1. It returns the value of the first element that passes test. 
+// 2. It executes a function for each array element. 
+// 3. It returns undefined if no elements are found.
+// 4. It does not execute the function for empty element.
+// 5. It does not change the original array
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { getProducts } from "../controller/product-controller.js";
 
 import { userSignup, userLogin } from "../controller/user-controller.js";
 
 
 const router = express.Router();
 
+router.get("/products", getProducts)
 
 router.post("/signup", userSignup);
 router.post("/login", userLogin);
